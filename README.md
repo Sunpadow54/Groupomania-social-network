1a19de104a44c3c8ab021dca5da1a46cb345ca09
elsa <elsa.dessarps@gmail.com>
2021-08-04 15:16:21 +0200

    Start the Project Groupomania intenal social network api

---
+++ README.md
@@ -0,0 +1,42 @@
+# Groupomania
+
+## *Projet de formation n°7 du parcours Développeur Web - Openclassrooms*
+### Créez un réseau social d’entreprise.
+
+-----------------
+
+## Mission
+
+Construire un réseau social interne pour l'entreprise *Groupomania*.
+
+* Personnaliser le contenu envoyé à un client web.
+* Authentifier un utilisateur et maintenir sa session.
+* Gérer un stockage de données à l'aide de SQL.
+* Implémenter un stockage de données sécurisé en utilisant SQL.
+
+
+***
+
+## :wrench: Prérequis
+
+### 1. Installation
+
+
+### 2. Paramétrer dotenv
+
+Créer un fichier <kbd> :page_facing_up: .env </kbd> dans le dossier <kbd> :file_folder: backend </kbd> & Changer les variables d’environnement
+
+
+### 3. Lancer le projet
+
+
+## :package: Made with
+
+* Node Js 
+    * Express
+* Database : mySql
+* Vue
+
+## Auteur
+
+Sunpadow - elsa dessarps - 2021
---
+++ backend/app.js
@@ -0,0 +1,52 @@
+// App
+// ------------------------- IMPORTS --------------------------
+
+const express = require('express'); // framework express
+const bodyParser = require('body-parser'); // Package (to format body (ex: from Post request) )
+const path = require('path'); // from node
+
+// ---- Import Roads
+const userRoutes = require('./routes/user');
+//const postRoutes = require('./routes/post');
+//const commentRoutes = require('./routes/comment');
+
+
+// ============================================================
+// ----------------------- Create app -------------------------
+
+const app = express();
+
+
+// ---------------- DB Connexion ---------------------
+
+
+
+// ------------------------- MIDDLEWARES ----------------------
+
+// ---- Create header (for CORS error)
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // all can access
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS'); // can use all theses methods
+    next();
+});
+
+// --- Format Json
+app.use(bodyParser.json());
+
+
+// --- middleware (for file upload path)
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+// --- Roads
+app.use('/api/auth', userRoutes);
+/* app.use('/api/post', postRoutes);
+app.use('/api/comment', commentRoutes); */
+
+
+
+// ============================================================
+// ------------------------- EXPORT ---------------------------
+
+module.exports = app; // our server node
