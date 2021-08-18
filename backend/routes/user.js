83dc94725b432c2c493c5c926d2a9b74aa8a1356
elsa <elsa.dessarps@gmail.com>
2021-08-18 16:20:20 +0200

    Feat (limiter) : add package 'express-rate-limit' to limit number of trial when connecting, and creating users

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,6 +7,7 @@ const express = require('express');
 const router = express.Router();
 
 // ---- Import middelwares
+const limiter = require('../middleware/limiter-config');
 const { userSignUpRules, validateSignUp } = require('../middleware/validate');
 
 // ----- Import Controlls
@@ -17,8 +18,8 @@ const userCtrl = require('../controllers/user');
 // ------------------------- ROADS ----------------------------
 
 
-router.post('/signup', /* limiter,*/ userSignUpRules(), validateSignUp , userCtrl.signup);
-router.post('/login', /* limiter, */ userCtrl.login);
+router.post('/signup', limiter, userSignUpRules(), validateSignUp , userCtrl.signup);
+router.post('/login', limiter, userCtrl.login);
 
 
 // ============================================================
